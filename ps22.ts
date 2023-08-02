11e466af66c26da8374c3c50b81b45fd7698adc9
Mariana Algafy <mariana.algafy@gmail.com>
2023-08-02 23:53:41 +0300

    PS22: All Star Code Challenge #18

---
+++ ps22.ts
@@ -0,0 +1,27 @@
+console.log("PS22: All Star Code Challenge #18");
+console.log('__________________________________________________________________________________________________')
+
+/*
+DESCRIPTION:
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+*/
+
+function past(h, m, s){
+    //#Happy Coding! ^_^
+    let oneSec = 1000;
+    let oneMin = 60 * oneSec;
+    let oneHour = 60 * oneMin;
+
+    return h*oneHour + m*oneMin + s*oneSec;
+}
+
+console.log(past(0,1,1));
