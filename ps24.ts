635a7ca2f0b17b0f6f038141e612ecf08e9e3f47
Mariana Algafy <mariana.algafy@gmail.com>
2023-08-03 19:19:46 +0300

    PS22: Will You Make It

--- ps24.ts
+++ ps24.ts
@@ -23,5 +23,5 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     }
 };
 
-console.log(zeroFuel(50, 25, 2))
-console.log(zeroFuel(100, 50, 1))
+console.log(zeroFuel(50, 25, 2)) // true
+console.log(zeroFuel(100, 50, 1)) // false
